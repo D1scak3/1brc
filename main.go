0daefb33318101f517d776416a39a5d04b81d2eb
Miguel Fazenda <miguel.fazenda2000@gmail.com>
2024-03-26 18:08:23 +0000

    fix: removed comment lines from csv, added store logic for each measure

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func CheckError(err error) {
@@ -14,7 +16,7 @@ func CheckError(err error) {
 
 func main() {
 
-	// open file
+	// handle file
 	file, err := os.Open("data/weather_stations.csv")
 	CheckError(err)
 	defer file.Close()
@@ -23,10 +25,37 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
+	// map for storing stations
+	stations := make(map[string][]float64)
+
 	// loop through file
 	for scanner.Scan() {
+
+		// split line into key and value
 		line := scanner.Text()
-		fmt.Println(line)
+		contents := strings.Split(line, ";")
+		key := contents[0]
+		value, err := strconv.ParseFloat(contents[1], 64)
+		CheckError(err)
+
+		// add key and value to map
+		if _, ok := stations[key]; !ok {
+			stations[key] = []float64{value, 1, value, value} // [cum_temp, count, min, max]
+		} else {
+			stations[key] = append(stations[key], value)
+			stations[key][1] += 1
+			if stations[key][2] > value {
+				stations[key][2] = value
+			}
+			if stations[key][3] < value {
+				stations[key][3] = value
+			}
+		}
+
+	}
+
+	for key, value := range stations { // round(float64(m.min)/10.0)
+		fmt.Printf("%s=%f/%f/%f\n", key, value[2], value[0]/value[1], value[3])
 	}
 
 }
